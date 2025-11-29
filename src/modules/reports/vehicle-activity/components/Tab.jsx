148acf82061b5555bd3c7e645da7b73aa7231785
Prajwal Tikhe <prajwaltikhe10@gmail.com>
2025-11-29 22:31:07 +0530

    Geofence Entry Exit & Emergency Alert updated

--- src/modules/reports/vehicle-activity/components/Tab.jsx
+++
@@ -1,8 +0,0 @@
-const tabs = [
-  { name: 'Movement', path: '/report/movement' },
-  { name: 'Parked', path: '/report/parked' },
-  { name: 'Idle', path: '/report/idle' },
-  { name: 'Offline', path: '/report/offline' },
-  { name: 'New-Device', path: '/report/new-device' },
-];
-export default tabs;
--- src/services/index.js
+++
@@ -1,4 +0,0 @@
-export { default as ApiService } from './ApiService';
-export { default as Lastvehicledata } from './lastVehicleData';
-export { default as AuthService } from './AuthService';
-export { default as AddressServices } from './AddressServices';
