2d942347fcdf898aefb66d8c0d4b9fa1a3562138
Prajwal Tikhe <72973991+prajwaltikhe@users.noreply.github.com>
2025-12-08 17:42:38 +0530

    Update MheStatusPanel.jsx

--- src/modules/multitrack/components/MheStatusPanel.jsx
+++ src/modules/multitrack/components/MheStatusPanel.jsx
@@ -102,7 +102,7 @@ const MheStatusPanel = ({ handleRightPanel, isShowPanel, vehicle }) => {
             ))}
           </div>
           <div className='flex flex-wrap justify-center gap-3 mt-4 w-full'>
-            <Link to={`/report/${status || ''}`}>
+            <Link to={status === 'New' ? '/report/new-device' : `/report/${status || ''}`}>
               <Btn>Reports</Btn>
             </Link>
             <Link to={`/management/vehicle-route`}>
